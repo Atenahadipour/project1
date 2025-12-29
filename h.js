e99311218ddf372835840f563593b236159787ca
Atenahadipour <usaiwu1384@gmail.com>
2025-12-29 09:40:55 +0330

    Update h.js

--- h.js
+++ h.js
@@ -1,14 +1,1 @@
-Changes not staged for commit:
-  (use "git add/rm <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-        deleted:    __pycache__/index.html
-        deleted:    __pycache__/numpy.cpython-312.pyc
-        deleted:    __pycache__/styles.css
-        deleted:    __pycache__/v.gitignore
-        deleted:    script.js
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-        css/
-        index.html
-        js/
+git commit -m "Initial commit – Add HTML, CSS, JS files"
