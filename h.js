19593035662f7d53496926315119ec3f1bd258df
Atenahadipour <usaiwu1384@gmail.com>
2025-12-29 09:46:13 +0330

    Update h.js

--- h.js
+++ h.js
@@ -1,10 +1,1 @@
-git push -u origin main
-To https://github.com/Atenahadipour/project2
- ! [rejected]        main -> main (fetch first)
-error: failed to push some refs to 'https://github.com/Atenahadipour/project2'
-hint: Updates were rejected because the remote contains work that you do not
-hint: have locally. This is usually caused by another repository pushing to
-hint: the same ref. If you want to integrate the remote changes, use
-hint: 'git pull' before pushing again.
-hint: See the 'Note about fast-forwards' in 'git push --help' for details.
-                               
+git pull origin main --allow-unrelated-histories
